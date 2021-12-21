bd50c1bebb73417163bbe9c8a043a7758310ac78
Oliver Ullman <oliverullman@gmail.com>
2021-12-21 12:42:27 -0300

    about to do sorting for lesson/sublesson

--- api/lesson/models/lesson.js
+++ api/lesson/models/lesson.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
+
+const { enforceSublessonChallengeSortOrder } = require("./sublesson.utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeUpdate(params, data) {
+      console.log("params", params, "data", data);
+      // await enforceSublessonChallengeSortOrder(
+      //   params.id,
+      //   data.sublesson_challenges_test,
+      //   (newIds) => (data.sublesson_challenges_test = newIds)
+      // );
+    },
+  },
+};
